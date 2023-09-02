2b7d9951eddfc49a042d1d88c3e0437b5761444d
Gustavo Mendes <gusttavo.me@outlook.com>
2023-09-02 03:01:12 +0000

    Add extras for cheetah-user 13 TQ3A.230805.001.A2 10385117 release-keys
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

---
+++ README.md
@@ -0,0 +1,14 @@
+# cheetah-user 13 TQ3A.230805.001.A2 10385117 release-keys
+- manufacturer: google
+- platform: gs201
+- codename: cheetah
+- flavor: cheetah-user
+- release: 13
+- id: TQ3A.230805.001.A2
+- incremental: 10385117
+- tags: release-keys
+- fingerprint: google/cheetah/cheetah:13/TQ3A.230805.001.A2/10385117:user/release-keys
+- is_ab: true
+- brand: google
+- branch: cheetah-user-13-TQ3A.230805.001.A2-10385117-release-keys
+- repo: google_cheetah_dump
---
+++ aosp-device-tree/device.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-impl \
+    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-service
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    disable_contaminant_detection.sh \
+    init.insmod.sh \
+    init.radio.sh \
+    init.uwb.calib.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.persist \
+    init.cheetah.rc \
+    init.gs201.rc \
+    init.gs201.usb.rc \
+    init.pantah.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.persist:$(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk/fstab.persist
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 33
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/google/cheetah/cheetah-vendor.mk)
